74d3271bc558155622bb87aa7012f3544d5e2e54
linsir <linsir678@gmail.com>
2018-01-30 17:52:12 +0800

    🚧 工作过程中:增加typeorm相关内容

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,5 +1,5 @@
 import {EggAppConfig} from "egg";
-
+import {join} from 'path';
 declare module 'egg' {
     export interface EggAppConfig {
 
@@ -14,6 +14,27 @@ export default (appInfo: EggAppConfig) => {
         level: 'NONE',
         consoleLevel: 'INFO',
         disableConsoleAfterReady: false,
+    };
+
+    config.security = {
+        csrf: false,
+        xframe: false,
+    };
+
+    config.typeorm = {
+        type: 'mysql',
+        host: '127.0.0.1',
+        database: 'JD-shopping-mall-DB',
+        username: 'root',
+        password: 'root',
+        logging: true,
+        charset: 'UTF8MB4_UNICODE_CI',
+        synchronize: true,
+        dropSchema: true,
+        entities: [
+            join(appInfo.baseDir, 'app/model/*.js')
+        ],
+
     };
 
     return config;
