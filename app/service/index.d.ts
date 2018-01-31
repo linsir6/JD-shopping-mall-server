ee196d2451921b66a88506318a9c70d873d40ff3
linsir <linsir678@gmail.com>
2018-01-31 15:02:49 +0800

    🚧 工作过程中: 添加实现声明

--- app/service/index.d.ts
+++ app/service/index.d.ts
@@ -2,7 +2,11 @@ import UserService from './UserService';
 
 declare module 'egg' {
 
+    import GoodsService from "./GoodsService";
+
     export interface IService {
         userService: UserService;
+        goodsService: GoodsService;
+
     }
 }
